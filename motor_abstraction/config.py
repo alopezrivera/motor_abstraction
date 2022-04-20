a603142730078b0178d2bed2f398267d70f5ce30
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2022-04-20 23:10:39 +0200

    getattr method

--- motor_abstraction/config.py
+++ motor_abstraction/config.py
@@ -10,13 +10,14 @@ def load(robot):
 
     # Initialize motors
     motors = []
+    getmod = lambda module: import_module(f"motor_abstraction.{module}")
     for motor_config in robot.values():
         
         # Communication protocol
         _protocol_confg = {k: motor_config.pop(k) for k in motor_config.keys() & {'topic', 'freq', 'generate_bindings'}}
-        _protocol_class = getattr(import_module('communicator'), f"{motor_config.pop('protocol')}")
+        _protocol_class = getattr(getmod('communicator'), f"{motor_config.pop('protocol')}")
         # Motor class
-        _motor_class    = getattr(import_module(motor_config.pop('manufacturer')), motor_config.pop('model'))
+        _motor_class    = getattr(getmod(motor_config.pop('manufacturer')), motor_config.pop('model'))
         # Initialize
         _protocol       = _protocol_class(**_protocol_confg)
         _motor          = _motor_class(protocol=_protocol, **motor_config)
