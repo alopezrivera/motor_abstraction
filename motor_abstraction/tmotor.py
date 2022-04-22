d1ecaa4285dcff6c983428227b992a1d50dd0311
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2022-04-22 11:23:45 +0200

    tmotor draft

--- motor_abstraction/tmotor.py
+++ motor_abstraction/tmotor.py
@@ -12,35 +12,52 @@ from motor_abstraction.utils.fallback import fallback_disable
 
 class ak80_6(AbstractMotor):
 
-    def __init__(self, can_port, motor_id, **kwargs):
+    # Motor characteristics
+    torque_max = 12
+
+    def __init__(self,
+                 protocol,
+                 can_port, motor_id,
+                 kp, kd,
+                 **kwargs):
         """
-        **T-Motor ak80-6**
+        **tmotor ak80-6**
         """
 
         self.motor_id = motor_id
-        
+
+        # Communication protocol
+        self.protocol = protocol
+
+        # Configuration
+        self.kp = kp
+        self.kd = kd
+        self.torque_limit = max(kwargs.pop('torque_limit'), ak80_6.torque_max) if 'torque_limit' in kwargs.keys() else ak80_6.torque_max
+
+        # Rest state
+        if 'rest_state' in kwargs.keys():
+            self.rest_state(kwargs.pop('rest_state'))
+
         # Driver
-        self.motor = CanMotorController(can_port, motor_id, **kwargs)
+        self.motor = CanMotorController(can_port, motor_id)
 
     def enable(self):
         self.motor.enable_motor()
     
-    @fallback_disable("setting rest position")
-    def rest(self):
-        return self.motor.send_deg_command(self.x_r)
+    @fallback_disable("zeroing motor")
+    def zero(self):
+        """
+        Zero at current motor position
+        """
+        pass
     
     @fallback_disable("sending command")
     def command(self, u):
-        return self.motor.send_rad_command(u)
+        return self.motor.send_rad_command(u[0], u[1], self.kp, self.kd, u[2])
+
+    @fallback_disable("setting rest position")
+    def rest(self):
+        return self.motor.send_rad_command(self.x_r)
 
     def disable(self):
         self.motor.disable_motor()
-
-# # Initialize
-# motor_1 = ak80_6(can_port="can0", motor_id=0x00)
-# # Rest state
-# motor_1.rest_state([0, 0, 0, 0, 0])
-# # Enable
-# motor_1.enable()
-# # Rest
-# motor_1.rest()
---
+++ tests/test_tmotor.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('../motor_abstraction')
+
+from motor_abstraction.config import load
+
+motor_1, motor_2 = load('tests/robot_tmotor.yml')
+
+# Enable
+motor_1.enable()
+# Rest
+motor_1.rest()
+# Command
+motor_1.command([1, 1, 1, 1, 1])
+# Disable
+motor_1.disable()
+
+
+# motor_1.listen()
